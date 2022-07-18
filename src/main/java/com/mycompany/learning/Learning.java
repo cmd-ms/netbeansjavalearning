060606ee4a2f509131f4e32ce67b9e06a233494c
Murat Sahin <murat_58sahin@hotmail.com>
2022-07-18 22:45:28 +0100

    first initial commit

---
+++ src/main/java/com/mycompany/learning/Learning.java
@@ -0,0 +1,12 @@
+package com.mycompany.learning;
+
+/**
+ *
+ * @author Murat
+ */
+public class Learning {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
