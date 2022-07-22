29173747f12e02ecc3cfd577f7cea9f8bb366a41
Murat Sahin <murat_58sahin@hotmail.com>
2022-07-22 23:22:20 +0100

    switch

--- src/main/java/com/mycompany/learning/Learning.java
+++ src/main/java/com/mycompany/learning/Learning.java
@@ -7,9 +7,46 @@ package com.mycompany.learning;
 public class Learning {
 
     public static void main(String[] args) {
+        /* BooleanExpression ? Expr1 : Expr2
         int time = 20;
-        String result = (time < 18)?"Good day":"Good evening.";
-        System.out.println(result);
-        System.out.println("hello");
+        String result = (time < 18)?"Good day":"Good evening.";      
+        System.out.println(result); */
+
+        /*
+        int day = 4;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+        } */
+// Outputs "Thursday" (day 4)
+/* The break keyword
+When Java reaches a break keyword, it breaks out of the switch block.
+This will stop the execution of more code and case testing inside the block.
+When a match is found, and the job is done, it's time for a break.
+There is no need for more testing. */
+
+
+
+
+
     }
 }
