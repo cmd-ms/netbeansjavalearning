e035e0de1d88d58cc1f12e62c7acf25f86a11102
Murat Sahin <murat_58sahin@hotmail.com>
2022-07-22 23:33:59 +0100

    switch new class

---
+++ src/main/java/com/mycompany/learning/Switch.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.learning;
+
+/**
+ *
+ * @author Murat
+ */
+public class Switch {
+
+    public static void main(String[] args) {
+        int day = 4;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+        }
+// Outputs "Thursday" (day 4)
+/* The break keyword
+When Java reaches a break keyword, it breaks out of the switch block.
+This will stop the execution of more code and case testing inside the block.
+When a match is found, and the job is done, it's time for a break.
+There is no need for more testing. */
+
+        int day1 = 4;
+        switch (day1) {
+            case 6:
+                System.out.println("Today is Saturday");
+                break;
+            case 7:
+                System.out.println("Today is Sunday");
+                break;
+            default:
+                System.out.println("Looking forward to the Weekend");
+        }
+// Outputs "Looking forward to the Weekend"
+/*The default Keyword
+The default keyword specifies some code to run if there is no case match:
+Note that if the default statement is used as the last statement in a switch block, it does not need a break. */
+
+    }
+}
